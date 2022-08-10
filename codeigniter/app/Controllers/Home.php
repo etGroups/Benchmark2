c1382771ea557ae743375c84bf688b47f8e834c0
emil-1996 <emil@dworniczak.pl>
2022-08-10 16:53:03 +0200

    add missing content type

--- codeigniter/app/Controllers/Home.php
+++ codeigniter/app/Controllers/Home.php
@@ -16,6 +16,7 @@ class Home extends BaseController
 	}
 	public function SqlHTTP()
 	{
+		$this->response->setHeader('Content-type', 'application/json');
 		return json_encode((new CustomerModel())->findAll(10));
 	}
 }
