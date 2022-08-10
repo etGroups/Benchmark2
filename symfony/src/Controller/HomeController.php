c0c45b3d597cf242095fd1a9eab492a845623b27
emil-1996 <emil@dworniczak.pl>
2022-08-10 14:51:09 +0200

    add Pong http to symfony

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -11,5 +12,18 @@ class HomeController extends AbstractController
 	public function HelloHTTP()
 	{
 		return new Response('Hello World', Response::HTTP_OK, ['content-type' => 'text/html']);
+	}
+
+	#[Route('/PongHTTP')]
+	public function PongHTTP(Request $request)
+	{
+		return new Response($request->getContent(), Response::HTTP_OK, ['content-type' => 'text/html']);
+	}
+
+	#[Route('/SqlHTTP')]
+	public function SqlHTTP()
+	{
+		$customers = ['test'];
+		return new Response(json_encode($customers), Response::HTTP_OK, ['content-type' => 'application/json']);
 	}
 }
